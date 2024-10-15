cb826b1000515f89d7efe2154f5187ef55c8f98b
Kirill <nikak.kirya@mail.ru>
2024-10-15 18:58:45 +0300

    refactor: fix User model

--- src/users/models.py
+++ src/users/models.py
@@ -13,5 +13,4 @@ class User(Base):
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    registration_date: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False,
-    server_default=func.now())
+    registration_date: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())

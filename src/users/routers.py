21e56fc41de57bd4ee3323275ea943eeaef6f467
Kirill <nikak.kirya@mail.ru>
2024-10-15 16:06:22 +0300

    feat: refactor router Post /users

--- src/users/routers.py
+++ src/users/routers.py
@@ -19,20 +19,7 @@ async def get_user_statistics(domain: str):
 @router.post("/users", response_model=UserFromDB)
 async def create_user(user: UserCreate):
     user_data = user.dict()
-
-    existing_user_by_email = await UserDao.find_by_email(user_data['email'])
-    if existing_user_by_email:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    existing_user_by_username = await UserDao.find_by_username(user_data['username'])
-    if existing_user_by_username:
-        raise HTTPException(status_code=400, detail="Username already taken")
-
-    try:
-        new_user = await UserDao.add_in_db(user_data)
-        return UserFromDB.from_orm(new_user)
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return await UserService.create_user(user_data)
 
 
 @router.get("/users", response_model=list[UserFromDB])

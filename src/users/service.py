d1a3237eb082f1ab1e420d16732e47a7515261dc
Kirill <nikak.kirya@mail.ru>
2024-10-15 16:01:47 +0300

    feat: refactor service.py

--- src/users/service.py
+++ src/users/service.py
@@ -31,3 +31,19 @@ class UserService:
             "top_5_users": [UserFromDB.from_orm(user) for user in top_5_users],
             "domain_share": domain_share
         }
+
+    @staticmethod
+    async def create_user(user_data: Dict[str, Any]) -> UserFromDB:
+        existing_user_by_email = await UserDao.find_by_email(user_data['email'])
+        if existing_user_by_email:
+            raise HTTPException(status_code=400, detail="Email already registered")
+
+        existing_user_by_username = await UserDao.find_by_username(user_data['username'])
+        if existing_user_by_username:
+            raise HTTPException(status_code=400, detail="Username already taken")
+
+        try:
+            new_user = await UserDao.add_in_db(user_data)
+            return UserFromDB.from_orm(new_user)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
